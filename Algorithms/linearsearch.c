34fd2a80cd67f92dc26baf45a21a972f709f747f
Yash Aggarwal <72155128+yashaggarwal03@users.noreply.github.com>
2020-10-02 23:11:15 +0530

    Alternative Program for Linear Search
    Please also take this alternative program for linear search in consideration. It includes taking array as an input from user. It is very simple program.

--- Algorithms/linearsearch.c
+++ Algorithms/linearsearch.c
@@ -1,22 +1,28 @@
-#include <stdio.h> 
-  
-int search(int arr[], int n, int x) 
-{ 
-    int i; 
-    for (i = 0; i < n; i++) 
-        if (arr[i] == x) 
-            return i; 
-    return -1; 
-} 
-  
-int main(void) 
-{ 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
-    int x = 10; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int result = search(arr, n, x); 
-    (result == -1) ? printf("Element is not present in array\n") 
-                   : printf("Element is present at index %d\n", 
-                            result); 
-    return 0; 
-} 
+#include<stdio.h>
+
+void main()
+{
+int ar[100],i,n,s=0,flag=0,c=0;
+printf("\n Enter no. of elements "); // taking no. of elements for array
+scanf("%d",&n);
+printf("\n Enter the numbers "); // taking array as an input from user
+//for loop for entering numbers  
+for(i=0;i<n;i++)
+{ scanf("%d",&ar[i]);
+}
+//loop for searching in array
+printf("\n Enter the number to search ");
+scanf("%d",&s);
+for(i=0;i<n;i++)
+{ if(s==ar[i])
+  {flag=1;
+  break;
+  }
+}
+if(flag==1)
+  printf("\n The number is present at position %d ",i+1);
+else
+  printf("\n The number is not present");
+
+  return 0;
+}
